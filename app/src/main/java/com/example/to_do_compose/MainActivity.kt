4ee5656ae15f454995965177c78fcad84fb070cf
CatAvadani <catalinaavadani@gmail.com>
2023-02-01 05:04:18 +0100

    Adding the  dependencies for the project

--- app/src/main/java/com/example/to_do_compose/MainActivity.kt
+++ app/src/main/java/com/example/to_do_compose/MainActivity.kt
@@ -3,12 +3,8 @@ package com.example.to_do_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.to_do_compose.ui.theme.TodocomposeTheme
 
@@ -17,27 +13,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TodocomposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp()
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    TodocomposeTheme {
-        Greeting("Android")
-    }
+fun MyApp() {
+    Text(text = "Catalina")
 }
